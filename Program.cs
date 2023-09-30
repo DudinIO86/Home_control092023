2f29b18a582c291effa64037c0ba7d08c736f814
Igor Dudin <dudinio@mail.ru>
2023-09-30 18:16:07 +0300

    Готовый код

--- Program.cs
+++ Program.cs
@@ -14,8 +14,45 @@ string[] EnterStrMassive(int number)//функция ввода исходног
 return StrArray;
 }
 
+string[] VerifyArray(string[] array)//создание нового массива, в который будут помещены отсортированные элементы
+{
+    int count = NumberElements(array);
+ 
+    string[] SortArray = new string[count]; 
+
+    int index = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3 && array[i]!=" ")
+        {
+            SortArray[index] = array[i];
+            index++;
+        }
+        
+    }
+    return SortArray;
+}
+
+int NumberElements(string[] array)//определение кол-ва элементов в массиве, у которых число знаков меньше или равно 3
+{
+int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3 && array[i]!=" ")
+        {
+            count++;
+        }
+    }
+return count;
+}
+
 System.Console.WriteLine("Введите число элементов массива:");
 int numberArray=int.Parse(Console.ReadLine());
 
 string[] MyArray=EnterStrMassive(numberArray);
+string[] NewArray=VerifyArray(MyArray);
+
 System.Console.WriteLine($"Мой исходный массив: [{string.Join(", ",MyArray)}]");
+System.Console.WriteLine($"Элеметы, число символов которых меньше или равно 3: [{string.Join(", ",NewArray)}]");
